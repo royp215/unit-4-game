cc8645f57e5a20463af28e3456b290bd54d50ca2
Roy Park <donghuip90@gmail.com>
2019-03-25 21:07:19 -0500

    links added to header for js and css

--- assests/javascript/game.js
+++ assests/javascript/game.js
@@ -1,8 +1,3 @@
-<!-- JAVASCRIPT  -->
-  <!-- ========================================= -->
-  <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-
-  <script type="text/javascript">
 
   var targetNumber = 53;
 
@@ -62,5 +57,3 @@
     }
 
   });
-
-  </script>
